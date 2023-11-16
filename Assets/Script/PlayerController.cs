3e5ec3a1ba7945869415c48bb76de72b44812b35
mongkeee <mohd.nurarif88@gmail.com>
2023-11-16 22:35:51 +0700

    update player movement, add player animation

--- Assets/Script/PlayerController.cs
+++ Assets/Script/PlayerController.cs
@@ -4,51 +4,33 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-
-    // inisisasi nilai 
-    float horizontalAxis;
-    float verticalAxis;
-    Vector2 direction;
-    Vector2 direction2;
-
-    [SerializeField]
-    private float speed;
-    // [SerializeField] private float speeds; gak kepake
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [SerializeField] private float speed = 2f;
+    [SerializeField] private Rigidbody2D rb;
+    [SerializeField] private Animator anim;
+    Vector2 move;
 
     // Update is called once per frame
     void Update()
     {
-        movementHorizontal();
-        movementVertical();
+        //buat cek input di keyboard/controller
+        move.x = Input.GetAxis("Horizontal");
+        move.y = Input.GetAxis("Vertical");
+
+        //buat animasi
+        if (move.sqrMagnitude < 0.01) //ngecek kecepatan player, kalo berhenti true, bergerak false
+        { 
+            anim.speed = 0; //ngebikin animasinya berhenti pas gak gerak
+        }
+        else
+        {
+            anim.SetFloat("Horizontal", move.x); //ngeset parameter yang ada di animator
+            anim.SetFloat("Vertical", move.y);
+            anim.speed = 1;
+        }
     }
 
-    private void movementHorizontal()
+    private void FixedUpdate() //beda dari void update karna Update ngubahnya perframe komputer, jadi tiap pc bakal beda kecepatannya
     {
-        horizontalAxis = Input.GetAxis("Horizontal"); //untuk membuat perpindahan bisa menggunakan inputan 
-        
-
-        direction = new Vector2(horizontalAxis, 0); //titik kordinat untuk perpindahan horizontal dimulai dari center atau 0
-        direction2 = new Vector2(0, verticalAxis);
-
-        speed = 3; // kecepatan perpindahan 
-
-
-        transform.Translate(direction * Time.deltaTime * speed); // code untuk mengeksekusi kecepatan yang dibuat, deltatime salah satu variabel untuk membuat perpindahan tisakmengikuti frame
-        transform.Translate(direction2 * Time.deltaTime * speed);
-
-    }
-
-    private void movementVertical()
-    {
-        verticalAxis = Input.GetAxis("Vertical");
-        direction2 = new Vector2(0, verticalAxis);
-
-        speed = 3; // awalnya make var speeds
-        // transform.Translate(direction2 * Time.deltaTime * speeds); gak kepake, kalo kepake jadi nambah kecepatan vertikalnya
+        rb.MovePosition(rb.position + move * speed * Time.fixedDeltaTime); //yang bikin karakter gerak, dikali Time.fixedDeltaTime biar lebih stabil gerakannya
     }
 }
