752dd1a1b956c7612b0ccee709f882c17b6bf104
leonza26 <muhammadleonza10@gmail.com>
2023-12-10 23:48:00 +0700

    add resume game an music controller

--- Assets/Script/AnimatorController.cs
+++ Assets/Script/AnimatorController.cs
@@ -6,14 +6,15 @@ public class AnimatorController : MonoBehaviour
 {
     [SerializeField] private Animator animator;
 
-
     public void Open()
     {
         animator.SetBool("IsOpen", true);
+        Time.timeScale = 0f; // Pause game
     }
 
     public void Close()
     {
         animator.SetBool("IsOpen", false);
+        Time.timeScale = 1f; // Kembalikan ke normal
     }
 }
