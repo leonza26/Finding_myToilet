4d47804b21cbc505dca4e69bed5085fedcf37ca7
leonza26 <muhammadleonza10@gmail.com>
2023-12-16 14:56:25 +0700

    add Vissual Novel 2

--- Assets/Script/InteraksiScenes.cs
+++ Assets/Script/InteraksiScenes.cs
@@ -25,7 +25,47 @@ public class InteraksiScenes : MonoBehaviour
         SceneManager.LoadScene("LevelEasy_1");
     }
 
+    public void level2easy()
+    {
+        SceneManager.LoadScene("LevelEasy_2");
+    }
 
+
+    public void level3easy()
+    {
+        SceneManager.LoadScene("LevelEasy_3");
+    }
+
+    public void level1Medium()
+    {
+        SceneManager.LoadScene("LevelMedium_1");
+    }
+
+    public void level2Medium()
+    {
+        SceneManager.LoadScene("LevelMedium_2");
+    }
+
+    public void level3Medium()
+    {
+        SceneManager.LoadScene("LevelMedium_3");
+    }
+
+
+    public void HometownVn1()
+    {
+        SceneManager.LoadScene("VisualNovel");
+    }
+
+    public void HometownVn2()
+    {
+        SceneManager.LoadScene("VisualNovel2");
+    }
+
+    public void HometownVn3()
+    {
+        SceneManager.LoadScene("VisualNovel3");
+    }
     public void Quit()
     {
         Application.Quit();
