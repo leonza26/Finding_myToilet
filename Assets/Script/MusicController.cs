6585a2dd70666e131e220ff2112b4d6b8530af43
mongkeee <mohd.nurarif88@gmail.com>
2023-12-19 12:21:59 +0700

    add sound settings, adjust props' Z,etc
    fix UI font's, fix BGM

--- Assets/Script/MusicController.cs
+++ Assets/Script/MusicController.cs
@@ -1,6 +1,9 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.IO;
+using System.Net;
+using UnityEditor.SearchService;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -8,14 +11,33 @@ public class MusicController : MonoBehaviour
 {
 
     public Slider asVolumeMusik;
+    public Slider asVolumeSound;
+
     public AudioSource asMusik;
+    public AudioSource asSoundPlayer;
+    public AudioSource asSoundManager;
+
     [SerializeField] AudioClip lagunya;
+
     // Start is called before the first frame update
     void Start()
     {
         asMusik = GameObject.Find("BGM").GetComponent<AudioSource>();
-        asVolumeMusik.value = asMusik.volume;
-        if(asMusik.clip != lagunya)
+
+        if(GameObject.Find("Manager/Player/Player") == null)
+        {
+            
+        }
+        else
+        {
+            asSoundPlayer = GameObject.Find("Manager/Player/Player").GetComponent<AudioSource>();
+            asSoundManager = GameObject.Find("GameManager").GetComponent<AudioSource>();
+        }
+
+        asVolumeMusik.value = asMusik.gameObject.GetComponent<audioSetting>().musicVolume;
+        asVolumeSound.value = asMusik.gameObject.GetComponent<audioSetting>().soundVolume;
+
+        if (asMusik.clip != lagunya)
         {
             asMusik.clip = lagunya;
             asMusik.Play();
@@ -30,6 +52,17 @@ public class MusicController : MonoBehaviour
 
     public void VolumeMusik()
     {
+        asMusik.gameObject.GetComponent<audioSetting>().musicVolume = asVolumeMusik.value;
         asMusik.volume = asVolumeMusik.value;
+    }
+
+    public void VolumeSound()
+    {
+        asMusik.gameObject.GetComponent<audioSetting>().soundVolume = asVolumeSound.value;
+        if(asSoundPlayer != null || asSoundManager != null)
+        {
+            asSoundPlayer.volume = asVolumeSound.value;
+            asSoundManager.volume = asVolumeSound.value;
+        }
     }
 }
---
+++ Assets/Script/audioSetting.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class audioSetting : MonoBehaviour
+{
+    public float musicVolume;
+    public float soundVolume;
+    // Start is called before the first frame update
+    void Start()
+    {
+        musicVolume = 1.0f;
+        soundVolume = 1.0f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
